55d8eedab550888ad2d099d4ae71f10427c8afbf
jpall <jpall@jpalldeMacBook-Pro.local>
2017-10-12 00:31:29 +0800

    CESHI

--- advanced/api/modules/v1/controllers/DefaultController.php
+++ advanced/api/modules/v1/controllers/DefaultController.php
@@ -22,4 +22,31 @@ class DefaultController extends Controller
 
         print_r("dsfsa");exit;
     }
+
+    public function actionReport(){
+        $app_name="ZLLBYS889J";
+        $app_key='NFUurmGyEo3m3LaspgbJ8U06e4BivBLxV9yKOPNjlfGWpMsnhljBNSTD8WyrlWvK';
+        $time_stamp = time();
+        $nonce = mt_rand();
+        $sign = calculate_sign($app_key, $time_stamp, $nonce);
+
+        $mobile = "13218029707";
+        $realname="中环";
+        $url = "http://t12.renaren.com/api/user/create?app_name=".$app_name."&time_stamp=".$time_stamp."&nonce=".$nonce."&sign=".$sign."&mobile=".$mobile."&realname=".$realname;
+        print_r($url);exit;
+    }
+
+    /**
+     * 返回参数加密签名Sign
+     *
+     * @param string $app_key,接入者密钥
+     * @param string $time_stamp,时间戳.自从 Unix 纪元（格林威治时间 1970 年 1 月 1 日 00:00:00）到当前时间的秒数
+     * @param string $nonce,随机数.9位随机整数
+     * @return  string
+     */
+    function calculate_sign($app_key, $time_stamp, $nonce) {
+        $tmpStr = $app_key.$time_stamp.$nonce;
+        $tmpStr = sha1($tmpStr);
+        return $tmpStr;
+    }
 }
